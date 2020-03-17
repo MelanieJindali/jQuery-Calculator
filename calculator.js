d55aa578f1c4c34c16106e46a7f25655cbbfd241
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-03-16 20:23:39 -0500

    Add functionality to buttons & operators

--- calculator.js
+++ calculator.js
@@ -17,8 +17,70 @@ $(document).ready(function() {
     var btnEqual = $("#button-equal");
     var btnClear = $("#button-clear");
     var firstNumberText = $("#first-number");
+    var operatorText = $("#operator");
 
+    // Assign on click functionality to number buttons
     btn1.on('click', function() {
         firstNumberText.append('1')
     })
+    btn2.on('click', function() {
+        firstNumberText.append('2')
+    })
+    btn3.on('click', function() {
+        firstNumberText.append('3')
+    })
+    btn4.on('click', function() {
+        firstNumberText.append('4')
+    })
+    btn5.on('click', function() {
+        firstNumberText.append('5')
+    })
+    btn6.on('click', function() {
+        firstNumberText.append('6')
+    })
+    btn7.on('click', function() {
+        firstNumberText.append('7')
+    })
+    btn8.on('click', function() {
+        firstNumberText.append('8')
+    })
+    btn9.on('click', function() {
+        firstNumberText.append('9')
+    })
+    btn0.on('click', function() {
+        firstNumberText.append('0')
+    })
+
+    // Assign on click functionality to operator buttons
+    btnPlus.on('click', function() {
+        operatorText.text('+')
+    })
+
+    btnMinus.on('click', function() {
+        operatorText.text('-')
+    })
+
+    btnDivide.on('click', function() {
+        operatorText.text('÷')
+    })
+
+    btnMultiply.on('click', function() {
+        operatorText.text('x')
+    })
+
+    btnPower.on('click', function() {
+        operatorText.text('^')
+    })
+
+    btnClear.on('click', function() {
+        
+    })
+
+    btnEqual.on('click', function() {
+        
+    })
+
+    // If operator is present, then add numbers to secondNumberText
+    // If operator is NOT present, then add numbers to firstNumberText
+    
 })
